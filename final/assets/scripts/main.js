a0fd9198c1734cb696f5aff86423960199ddf662
ravenusmc <mcuddy77@gmail.com>
2017-04-03 12:42:56 -0400

    added another function

--- final/assets/scripts/main.js
+++ final/assets/scripts/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+//The below Javascript will apply to all files
+
+//This function will allow the user to go back to their previous page.
+function goBack() {
+    window.history.back();
+}
+
+
+//Index Page code 
+//This function will change the image at the bottom of the Index page / Site Map section
 function changeSiteMapImage(){
   let target = document.getElementById('site_map_image');
 
@@ -11,3 +21,6 @@ function changeSiteMapImage(){
     target.style.color = 'black';
   }
 };
+
+
+
